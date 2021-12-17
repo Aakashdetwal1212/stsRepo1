7ace3822107686b96a8661b297e5cbca7f0f6538
aakash <aakashdetwal@gmail.com>
2021-12-17 09:38:58 +0530

    updated

---
+++ employee_registration_poc1/src/main/java/com/employee/exception/NoSernameResourceFoundException.java
@@ -0,0 +1,8 @@
+package com.employee.exception;
+
+public class NoSernameResourceFoundException extends RuntimeException {
+
+	public NoSernameResourceFoundException(String str) {
+		super(str);
+	}
+}
