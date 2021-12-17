054a0096e110ddc3dc7aa390d4eb32ea6a14e1ac
Aakashdetwal1212 <52354016+Aakashdetwal1212@users.noreply.github.com>
2021-12-17 09:13:02 +0530

    Delete employee_registration directory

--- employee_registration/src/main/java/com/employee/exception/NoPincodeResourceFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.employee.exception;
-
-public class NoPincodeResourceFoundException extends RuntimeException {
-
-	public NoPincodeResourceFoundException(String str) {
-		super(str);
-	}
-}
